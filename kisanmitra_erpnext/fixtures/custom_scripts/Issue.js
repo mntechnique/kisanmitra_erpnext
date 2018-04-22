add54e2095f9a8fbc01eaac8149201ef00114a63
Gaurav Naik <gvrvnk@gmail.com>
2018-04-22 21:25:59 +0530

    [Fix] Filter cascade for State, District, Mandal, Village

--- kisanmitra_erpnext/fixtures/custom_scripts/Issue.js
+++ kisanmitra_erpnext/fixtures/custom_scripts/Issue.js
@@ -2,17 +2,23 @@ frappe.ui.form.on("Issue", {
 	refresh: function(frm) {
 		frm.set_query("km_district_case", function() {
 			return {
-				"parent_territory": frm.doc.km_state_case
+				filters: {
+					"parent_territory": frm.doc.km_state_case
+				}
 			}
 		});
 		frm.set_query("km_mandal_case", function() {
 			return {
-				"parent_territory": frm.doc.km_district_case
+				filters: {
+					"parent_territory": frm.doc.km_district_case
+				}
 			}
 		});
 		frm.set_query("km_village_case", function() {
 			return {
-				"parent_territory": frm.doc.km_mandal_case
+				filters: {
+					"parent_territory": frm.doc.km_mandal_case
+				}
 			}
 		});
 	}
