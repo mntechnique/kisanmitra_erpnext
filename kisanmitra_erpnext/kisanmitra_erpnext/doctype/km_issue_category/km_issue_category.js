ef15120e0736c1cff8834176b473e4f1aabf08c5
Gaurav Naik <gvrvnk@gmail.com>
2018-05-14 14:16:58 +0530

    Basic doctype for case category on Issue

---
+++ kisanmitra_erpnext/kisanmitra_erpnext/doctype/km_issue_category/km_issue_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, MN Technique and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('KM Issue Category', {
+	refresh: function(frm) {
+
+	}
+});
