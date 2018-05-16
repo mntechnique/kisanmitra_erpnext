63650740c6ac290131bb2c8bd6d111b121ac7fe4
deepak-mnt <deepak@mntechnique.com>
2018-05-16 18:27:23 +0530

    minor changes

--- kisanmitra_erpnext/patches/update_lead_contact_field.py
+++ kisanmitra_erpnext/patches/update_lead_contact_field.py
@@ -10,14 +10,15 @@ def execute():
 		contact = ''
 		raised_by_phone = ''
 		caller_name = issue.km_caller_name
-		caller_name = (caller_name.split("."))[0].strip()+" 0"
-		lead = (frappe.get_all("Lead", filters={"company_name":caller_name}))[0].get("name")
-		contact = frappe.db.get_value("Dynamic Link",{"link_name":lead},"parent")
-		raised_by_phone = frappe.db.get_value("Contact",contact,"mobile_no")
-		if lead and contact and raised_by_phone:
-			frappe.db.sql("""update `tabIssue`
-					set lead=%s, contact=%s,
-					raised_by_phone=%s where name=%s""",(lead,contact,raised_by_phone,issue.get("name")))
+		if caller_name:
+			caller_name = (caller_name.split("."))[0].strip()+" 0"
+			lead = (frappe.get_all("Lead", filters={"company_name":caller_name}))[0].get("name")
+			contact = frappe.db.get_value("Dynamic Link",{"link_name":lead},"parent")
+			raised_by_phone = frappe.db.get_value("Contact",contact,"mobile_no")
+			if lead and contact and raised_by_phone:
+				frappe.db.sql("""update `tabIssue`
+						set lead=%s, contact=%s,
+						raised_by_phone=%s where name=%s""",(lead,contact,raised_by_phone,issue.get("name")))
 
 	frappe.db.sql("""update `tabIssue`
 					set raised_by_phone='' where contact=''""")
