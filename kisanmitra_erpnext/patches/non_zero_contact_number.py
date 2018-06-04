23500a9b6ae6f5d02f31a88d892aa4810f0df21d
Saurabh <bhosalesaurabh8@gmail.com>
2018-06-04 11:48:19 +0530

    Non-zero number patch

--- kisanmitra_erpnext/patches/non_zero_contact_number.py
+++ kisanmitra_erpnext/patches/non_zero_contact_number.py
@@ -5,37 +5,66 @@ def execute():
 	## Communication
 	# call receiver, exophone, phone_no
 	try:
-		communications = frappe.get_all("Communication", filters={"status":"Info"})
+		communications = frappe.get_all("Communication", fields=["*"], filters={"comment_type":"Info"})
 
 		for communication in communications:
-			frappe.db.sql("""update `tabComunication`
-				set call_receiver=%s,exophone=%s,phone_no=%s where name=%s""",\
-				(communication.get("call_receiver")[1:11],communication.get("exophone")[1:11],communication.get("phone_no")[1:11], communication.get("name")))
-			frappe.db.commit()
+			if(communication.get("call_receiver") and len(communication.get("call_receiver"))==11):
+				frappe.db.sql("""update `tabCommunication`
+				set call_receiver=%s where name=%s""",\
+				(communication.get("call_receiver")[1:11], communication.get("name")))
+				frappe.db.commit()
+
+			if(communication.get("exophone") and len(communication.get("exophone"))==11):
+				frappe.db.sql("""update `tabCommunication`
+					set exophone=%s where name=%s""",\
+					(communication.get("exophone")[1:11], communication.get("name")))
+				frappe.db.commit()
+
+			if(communication.get("phone_no") and len(communication.get("phone_no"))==11):
+				frappe.db.sql("""update `tabCommunication`
+				set phone_no=%s where name=%s""",\
+				(communication.get("phone_no")[1:11], communication.get("name")))
+				frappe.db.commit()
 	except Exception as e:
 		frappe.log_error(message=frappe.get_traceback(), title="Error in communication patch")
 
 	## Contact 
 	# mobile_no
 	try:
-		contacts = frappe.get_all("Contact", filters={})
+		contacts = frappe.get_all("Contact", fields=["*"], filters=[], order_by="creation desc")
 
 		for contact in contacts:
-			frappe.db.sql("""update `tabContact`
+			if(len(contact.get("mobile_no"))==11):
+				frappe.db.sql("""update `tabContact`
 				set mobile_no=%s where name=%s""",(contact.get("mobile_no")[1:11], contact.get("name")))
-			frappe.db.commit()
+				frappe.db.commit()
 	except Exception as e:
 		frappe.log_error(message=frappe.get_traceback(), title="Error in contact patch")
 
 	## Issue
 	# raised_by_phone
 	try:
-		issues = frappe.get_all("Issue", filters={})
+		issues = frappe.get_all("Issue", fields=["*"], filters=[])
 		
 		for issue in issues:
-			if(issue.raised_by_phone):
-				frappe.db.sql("""update `tabIssue`
-					set raised_by_phone=%s where name=%s""",(issue.get("raised_by_phone")[1:11], issue.get("name")))
-				frappe.db.commit()
+			if(issue.get("raised_by_phone")!=""):
+				if(len(issue.get("raised_by_phone"))==11):
+					frappe.db.sql("""update `tabIssue`
+						set raised_by_phone=%s where name=%s""",(issue.get("raised_by_phone")[1:11], issue.get("name")))
+					frappe.db.commit()
 	except Exception as e:
 		frappe.log_error(message=frappe.get_traceback(), title="Error in issue patch")
+
+	## User
+	# user's mobile_no
+	try:
+		users = frappe.get_all("User", fields=["*"], filters=[])
+		
+		for user in users:
+			if(user.get("mobile_no")!=""):
+				if(len(user.get("mobile_no"))==11):
+					frappe.db.sql("""update `tabUser`
+						set mobile_no=%s where name=%s""",(user.get("mobile_no")[1:11], user.get("name")))
+					frappe.db.commit()
+	except Exception as e:
+		frappe.log_error(message=frappe.get_traceback(), title="Error in User patch")
